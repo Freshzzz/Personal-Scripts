d6cfd58a649f688a2254ff1a6aa784bd883008e7
FreshPython <giedprof@gmail.com>
2024-12-14 21:45:07 +0200

    Added a new Window, 3 buttons
    Added a window, which opens when the user presses "Youtube Converter" button in the main menu
    Added 3 buttons:
       Youtube Converter - Opens a new window where the user will be able to convert their links to an mp3 file
       Back To Main - When in the "Youtube Converter" window, pressing the back to main button, will close the current window and open the main menu.
       Exit - This button completely closes the program.

--- Menu/mainMenu.py
+++ Menu/mainMenu.py
@@ -3,20 +3,42 @@ from tkinter import ttk
 import sv_ttk
 
 
-def youtube_converter():
-    print("Button Clicked")
-
+# Creates & Opens the Main Menu Window
 window = tk.Tk()
 window.title("Main Menu")
 window.geometry("1280x1080")
 sv_ttk.use_dark_theme()
+    
 
-style = ttk.Style()
-style.configure("TButton", foreground="blue", font=("Arial", 12))
+def main_menu(cwd):
+    cwd.destroy()
+    
+    window.deiconify()
 
-button = ttk.Button(window, text="Click Me", command=youtube_converter)
-button.place(x=10, y=10)
 
+
+# Opens a new window where the user can convert their youtube link to a mp3 file
+def youtube_converter():
+    window.withdraw()
+    youtube_window = tk.Toplevel()
+    youtube_window.title("Youtube Video To MP3 Converter")
+    youtube_window.geometry("1280x1080")
+    
+    exitButton = ttk.Button(youtube_window, text="Exit", command=destroy_program, width=20).pack(side="bottom", pady=10)
+    backButton = ttk.Button(youtube_window, text="Back To Main", command=lambda: main_menu(youtube_window)
+                            , width=20).pack(side="bottom", pady=10)
+    
+
+def destroy_program():
+    window.destroy()
+    
+
+style=ttk.Style()
+style.configure("TButton", fg="white", font=("Arial", 16))
+    
+button = ttk.Button(window, text="Youtube Converter", command=youtube_converter, width=40).place(x=25, y=25)
+exitButton = ttk.Button(window, text="Exit", command=destroy_program, width=10).pack(side="bottom", pady=10)
 
 window.mainloop()
+    
 
