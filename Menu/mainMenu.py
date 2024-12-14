70bb96550d11df0d8eb644f41e7eae717cbe5697
FreshPython <giedprof@gmail.com>
2024-12-14 20:16:39 +0200

    Main menu lango pradzia
    Atidaromas "Main menu langas", uždėtas "Dark" mode'as.
    Kolkas įdėtas tik 1 mygtukas, kuris išprintina "Button Clicked"

--- Menu/mainMenu.py
+++ Menu/mainMenu.py
@@ -1,1 +1,22 @@
 import tkinter as tk
+from tkinter import ttk
+import sv_ttk
+
+
+def youtube_converter():
+    print("Button Clicked")
+
+window = tk.Tk()
+window.title("Main Menu")
+window.geometry("1280x1080")
+sv_ttk.use_dark_theme()
+
+style = ttk.Style()
+style.configure("TButton", foreground="blue", font=("Arial", 12))
+
+button = ttk.Button(window, text="Click Me", command=youtube_converter)
+button.place(x=10, y=10)
+
+
+window.mainloop()
+
