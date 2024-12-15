68807811ebdeaaa68272bf5071e2d5f1c1fdf737
FreshPython <giedprof@gmail.com>
2024-12-15 13:12:17 +0200

    Grid Placement
    Both Scripts now use .grid placement instead of .pack or using coordinates (x, y)

--- Menu/mainMenu.py
+++ Menu/mainMenu.py
@@ -3,15 +3,19 @@ from tkinter import ttk
 import sv_ttk
 
 
-# Creates & Opens the Main Menu Window
-window = tk.Tk()
-window.title("Main Menu")
-window.geometry("1280x1080")
-sv_ttk.use_dark_theme()
+def configure_grid(window):
+    window.grid_rowconfigure(0, weight=0)
+    window.grid_rowconfigure(1, weight=1)
+    window.grid_rowconfigure(2, weight=1)
+    window.grid_rowconfigure(3, weight=0)
+    
+    window.grid_columnconfigure(0, weight=1)
+    window.grid_columnconfigure(1, weight=0) # Has 0 weight, when window expands, the buttons are fixed in the middle
+    window.grid_columnconfigure(2, weight=1)
 
 
 # Opens a new window where the user can convert their youtube link to a mp3 file
-def youtube_converter():
+def youtube_converter(window):
     window.withdraw()
     
     import subprocess
@@ -19,20 +23,50 @@ def youtube_converter():
     
 
 # Exits the program completely
-def destroy_program():
+def destroy_program(window):
     window.destroy()
     
 
-# Button Design Configuration
-style=ttk.Style()
-style.configure("TButton", fg="white", font=("Arial", 16))
+def main_menu():
+    # Creates & Opens the Main Menu Window
+    window = tk.Tk()
+    window.title("Main Menu")
+    window.geometry("1280x1080")
+    sv_ttk.use_dark_theme()
+    
+    configure_grid(window)
+    
+    # Button Design Configuration
+    style=ttk.Style()
+    style.configure("TButton",
+                    fg="white",
+                    font=("Arial", 16),
+                    padding=(0, 20))
     
 
-# Button Implementation
-button = ttk.Button(window, text="Youtube Converter", command=youtube_converter, width=40).place(x=25, y=25)
-exitButton = ttk.Button(window, text="Exit", command=destroy_program, width=10).pack(side="bottom", pady=10)
+    # Button Implementation
+    button = ttk.Button(window, text="Youtube Converter", command=lambda: youtube_converter(window),
+                        width=25).grid(row=0, column=0, pady=100)
+    button2 = ttk.Button(window, text="2", width=25).grid(row=0, column=1)
+    button3 = ttk.Button(window, text="3", width=25).grid(row=0, column=2)
+    
+    button4 = ttk.Button(window, text="4", width=25).grid(row=1, column=0)
+    button5 = ttk.Button(window, text="5", width=25).grid(row=1, column=1)
+    button6 = ttk.Button(window, text="6", width=25).grid(row=1, column=2)
+    
+    button7 = ttk.Button(window, text="7", width=25).grid(row=2, column=0)
+    button8 = ttk.Button(window, text="8", width=25).grid(row=2, column=1)
+    button9 = ttk.Button(window, text="9", width=25).grid(row=2, column=2)
+    
+    exitButton = ttk.Button(window, text="Exit", command=lambda: destroy_program(window)
+                            , width=10).grid(row=3, column=1, padx=10, pady=20)
 
+    window.mainloop()
+    
+    
 
-window.mainloop()
+if __name__ == "__main__":
+    main_menu()
+
     
 
--- Menu/youtube_converter.py
+++ Menu/youtube_converter.py
@@ -14,9 +14,21 @@ def main_menu(youtube_window):
     
     import subprocess
     subprocess.Popen(["python", "mainMenu.py"])
+    
+
+def configure_grid(window):
+    window.grid_rowconfigure(0, weight=1)
+    window.grid_rowconfigure(1, weight=1)
+    window.grid_rowconfigure(2, weight=0)
+    
+    window.grid_columnconfigure(0, weight=1)
+    window.grid_columnconfigure(1, weight=0) # Has 0 weight, when window expands, the buttons are fixed in the middle
+    window.grid_columnconfigure(2, weight=0)
+    window.grid_columnconfigure(3, weight=1)
 
 
 def start_youtube_converter():
+    
     
     # Window Configuration
     youtube_window = tk.Tk()
@@ -24,18 +36,27 @@ def start_youtube_converter():
     youtube_window.geometry("1280x720")
     sv_ttk.use_dark_theme()
     
+    configure_grid(youtube_window)
+    
     
     # Button Implementation/Configuration
     exitButton = ttk.Button(youtube_window, text="Exit", command=lambda: destroy_program(youtube_window)
-                            , width=20).pack(side="bottom", pady=10)
+                            , width=20).grid(row=2, column=1, padx=10, pady=20)
     backButton = ttk.Button(youtube_window, text="Back To Main", command=lambda: main_menu(youtube_window)
-                        , width=20).pack(side="bottom", pady=10)
+                        , width=20).grid(row=2, column=2, padx=10, pady=20)
     
     
     # Button Style Configuration
     style=ttk.Style()
     style.configure("TButton", fg="white", font=("Arial", 16))
     
+    
+    youtube_link = tk.StringVar()
+    link_label = tk.Label(youtube_window, text="Enter the Youtube link", font=("Arial", 16))
+    link_entry = tk.Entry(youtube_window, textvariable = youtube_link, font=("Arial", 16, 'normal'))
+    
+    #link_label.grid(row=0, column=0)
+    #link_entry.grid(row=0, column=1)
     
     
     youtube_window.mainloop()
