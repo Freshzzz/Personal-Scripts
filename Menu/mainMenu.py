daa11565f75b6a96d50b56eb653330ac0e07c17c
FreshPython <giedprof@gmail.com>
2024-12-17 12:30:59 +0200

    3 Scripts added to the main menu
    3 Scripts added to buttons 7 through 9

--- Menu/mainMenu.py
+++ Menu/mainMenu.py
@@ -1,7 +1,47 @@
 import tkinter as tk
 from tkinter import ttk
 import sv_ttk
+import os
+import subprocess
+import threading
 
+
+script_base_path = r"E:\Studijos\Praktika"
+
+
+def first_script():
+    def run_script():
+        script_1_path = os.path.abspath(os.path.join(script_base_path, "Script_1\SC1.bat"))
+        script_1_dir = os.path.dirname(script_1_path)
+        subprocess.run(
+            [script_1_path],
+            cwd=script_1_dir
+        )
+    threading.Thread(target=run_script, daemon=True).start()
+
+
+def second_script():
+    def run_script():
+        script_2_path = os.path.abspath(os.path.join(script_base_path, "Script_2\SC2.bat"))
+        script_2_dir = os.path.dirname(script_2_path)
+        subprocess.Popen(
+            ["cmd", "/c", "start", script_2_path],
+            cwd=script_2_dir
+        )
+        
+    threading.Thread(target=run_script, daemon=True).start()
+    
+    
+def third_script():
+    def run_script():
+        script_3_path = os.path.abspath(os.path.join(script_base_path, "Script_3\SC3.bat"))
+        script_3_dir = os.path.dirname(script_3_path)
+        subprocess.run(
+            ["cmd", "/c", "start", script_3_path],
+            cwd=script_3_dir
+        )
+    threading.Thread(target=run_script, daemon=True).start()
+        
 
 def configure_grid(window):
     window.grid_rowconfigure(0, weight=0)
@@ -54,9 +94,9 @@ def main_menu():
     button5 = ttk.Button(window, text="5", width=25).grid(row=1, column=1)
     button6 = ttk.Button(window, text="6", width=25).grid(row=1, column=2)
     
-    button7 = ttk.Button(window, text="7", width=25).grid(row=2, column=0)
-    button8 = ttk.Button(window, text="8", width=25).grid(row=2, column=1)
-    button9 = ttk.Button(window, text="9", width=25).grid(row=2, column=2)
+    button7 = ttk.Button(window, text="1st Script", command=first_script, width=25).grid(row=2, column=0)
+    button8 = ttk.Button(window, text="2nd Script", command=second_script, width=25).grid(row=2, column=1)
+    button9 = ttk.Button(window, text="3rd Script", command=third_script, width=25).grid(row=2, column=2)
     
     exitButton = ttk.Button(window, text="Exit", command=lambda: destroy_program(window)
                             , width=10).grid(row=3, column=1, padx=10, pady=20)
